eff9f116b2e861e646d2b0c33ebd1db0c1391c0d
cheyulin <yche@cse.ust.hk>
2016-08-12 19:52:20 +0800

    add user d hash m ap

--- pure_memory_key_value_store.h
+++ pure_memory_key_value_store.h
@@ -8,13 +8,9 @@
 #include <string>
 #include <fstream>
 #include <sstream>
-#include <unordered_map>
 #include <algorithm>
-#include <functional>
 #include <list>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
+#include <cstddef>
 
 using namespace std;
 
@@ -24,16 +20,46 @@ using namespace std;
 #define SEPERATOR_END_STRING ";"
 #define SEPERATOR_CHAR ','
 
+std::hash<string> str_hash_func;
+
+template<typename size_t slot_num = 100000>
+class yche_string_string_map {
+    vector<std::list<pair<string, string>>> my_hash_table_{slot_num};
+    size_t current_size_{0};
+
+public:
+    inline size_t size() {
+        return current_size_;
+    }
+
+    inline string find(const string &key) {
+        auto index = str_hash_func(key) % slot_num;
+        for (auto &my_pair:my_hash_table_[index]) {
+            if (my_pair.first == key) {
+                return my_pair.second;
+            }
+        }
+        return nullptr;
+    }
+
+
+    inline void insert_or_replace(const string &key, const string &value) {
+        auto index = str_hash_func(key) % slot_num;
+        for (auto &my_pair:my_hash_table_[index]) {
+            if (my_pair.first == key) {
+                my_pair.second = value;
+                return;
+            }
+        }
+        my_hash_table_[index].push_back(make_pair(key, value));
+        ++current_size_;
+    }
+
+};
+
 class Answer {
 private:
-    unordered_map<string, string> yche_map_;
-    list <function<void(void)>> io_function_obj_list_;
-    array<thread, 1> worker_threads_;
-    mutex job_queue_mutex_;
-    condition_variable wait_cond_var_;
-    bool is_finished_computation{false};
-    bool is_io_thread_idle{false};
-
+    yche_string_string_map<5000000> yche_map_;
     fstream output_file_stream_;
     size_t count{0};
     bool first_time{true};
@@ -42,34 +68,13 @@ private:
         auto iter_begin = str.begin();
         auto iter_end = str.end();
         auto iter_middle = find(iter_begin, iter_end, ',');
+
         return std::move(make_pair(std::move(string(iter_begin, iter_middle)),
                                    std::move(string(iter_middle + 1, iter_end - 1))));
     }
 
-    void Task() {
-        for (; !is_finished_computation;)
-            next_job()();
-    }
-
-    std::function<void(void)> next_job() {
-        std::function<void(void)> res;
-        std::unique_lock<std::mutex> job_lock(job_queue_mutex_);
-        for (; io_function_obj_list_.size() < 1;) {
-            wait_cond_var_.wait(job_lock);
-            is_io_thread_idle = true;
-        }
-        if (!is_finished_computation) {
-            res = io_function_obj_list_.front();
-            io_function_obj_list_.pop_front();
-        }
-        return res;
-    }
-
-
 public: //put和get方法要求public
     Answer() {
-        std::ios::sync_with_stdio(false);
-        yche_map_.reserve(5000000);
         ifstream input_file_stream{FILE_NAME, ifstream::in};
 
         if (input_file_stream.is_open()) {
@@ -86,7 +91,7 @@ public: //put和get方法要求public
                 getline(str_stream, tmp_string);
                 if (tmp_string.size() > 0 && tmp_string.substr(tmp_string.size() - 1) == SEPERATOR_END_STRING) {
                     auto my_pair = std::move(split(tmp_string));
-                    yche_map_[std::move(my_pair.first)] = std::move(my_pair.second);
+                    yche_map_.insert_or_replace(my_pair.first, my_pair.second);
                 }
             }
             delete[](file_content);
@@ -98,46 +103,34 @@ public: //put和get方法要求public
             first_time = false;
         }
         output_file_stream_.open(FILE_NAME, std::ofstream::out | std::ofstream::app);
-        worker_threads_[0] = std::move(thread([this]() { this->Task(); }));
-    }
-
-    virtual ~Answer() {
-        is_finished_computation = true;
-        wait_cond_var_.notify_one();
-        worker_threads_[0].join();
     }
 
     inline string get(string key) { //读取KV
-        auto iter = yche_map_.find(key);
-        if (iter != yche_map_.end()) {
-            return iter->second;
+        auto result = yche_map_.find(key);
+        if (result == nullptr) {
+            return "NULL"; //文件不存在，说明该Key不存在，返回NULL
         }
         else {
-            return "NULL"; //文件不存在，说明该Key不存在，返回NULL
+            return result;
         }
     }
 
     inline void put(string key, string value) { //存储KV
-        yche_map_[key] = value;
+        yche_map_.insert_or_replace(key, value);
         ++count;
-        io_function_obj_list_.emplace_back([key, value, this]() {
-            this->output_file_stream_ << key << SEPERATOR << value << SEPERATOR_END_CHAR << '\n';
-            if (this->first_time) {
-                if (this->yche_map_.size() < 100000) {
-                    output_file_stream_ << flush;
-                }
-                else {
-                    this->first_time = false;
-                    this->count = 0;
-                }
+        output_file_stream_ << key << SEPERATOR << value << SEPERATOR_END_CHAR << '\n';
+        if (first_time) {
+            if (yche_map_.size() < 100000) {
+                output_file_stream_ << flush;
             }
-            else if (this->count > 5000) {
-                this->output_file_stream_ << flush;
-                this->count = 0;
+            else {
+                first_time = false;
+                count = 0;
             }
-        });
-        if (is_io_thread_idle == true) {
-            wait_cond_var_.notify_one();
+        }
+        else if (count > 5000) {
+            output_file_stream_ << flush;
+            count = 0;
         }
     }
 };
