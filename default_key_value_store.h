1fad4747234b9b5e5a7aa4d79d728aa812f7d9c9
cheyulin <yche@cse.ust.hk>
2016-08-17 15:22:37 +0800

    adjust strategy

--- default_key_value_store.h
+++ default_key_value_store.h
@@ -8,49 +8,59 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-#include <memory>
 #include <unordered_map>
 
-#include <sys/stat.h>
-#include <dirent.h>
+#define INDEX_FILE_NAME "key_file_name_index_file.txt"
 
 using namespace std;
 
-template<typename _Tp, typename... _Args>
-inline unique_ptr<_Tp> make_unique(_Args &&... __args) {
-    return unique_ptr<_Tp>(new _Tp(std::forward<_Args>(__args)...));
-}
-
-std::hash<string> str_hash_func_basic;
-
 class Answer {
-    array<fstream, 1000> my_file_streams_;
-    array<bool, 1000> mark_flags_;
-    unordered_map<string, int> key_set_;
+private:
+    unordered_map<string, string> key_file_name_map_;
+//    map<string, string> key_value_map_;
+    fstream key_filename_stream_;
+    int file_name_num_{0};
 
 public:
     Answer() {
-        struct stat st = {0};
-        for (auto i = 0; i < 1000; i++) {
-            if (stat(to_string(i).c_str(), &st) == -1)
-                mkdir(to_string(i).c_str(), 0700);
+        key_filename_stream_.open(INDEX_FILE_NAME, ios::in | ios::out | ios::app);
+        string key_str;
+        string file_name_str;
+        for (; key_filename_stream_.good();) {
+            getline(key_filename_stream_, key_str);
+            if (key_filename_stream_.good()) {
+                getline(key_filename_stream_, file_name_str);
+                key_file_name_map_[key_str] = file_name_str;
+            }
         }
+        key_filename_stream_.clear();
     }
 
     string get(string key) {
-        ifstream is(to_string(str_hash_func_basic(key) % 1000) + "/" + key);
-        if (is.good()) {
+        if (key_file_name_map_.find(key) == key_file_name_map_.end()) {
+            return "NULL";
+        } else {
+            ifstream is(key_file_name_map_[key]);
             string value;
             is >> value;
+            is.close();
+//            if (key_value_map_.find(key) == key_value_map_.end()) {
+//                key_value_map_[key] = result_str;
+//            }
             return value;
-        } else {
-            return "NULL";
         }
     }
 
-    void put(string key, string value) { //存储KV
-        ofstream os(to_string(str_hash_func_basic(key) % 1000) + "/" + key);
-        os << value << flush;
+    void put(string key, string value) {
+        if (key_file_name_map_.find(key) == key_file_name_map_.end()) {
+            key_filename_stream_ << key << "\n" << to_string(file_name_num_) << "\n" << flush;
+            key_file_name_map_[key] = to_string(file_name_num_);
+            file_name_num_++;
+        }
+//        key_value_map_[key] = value;
+        ofstream os(key_file_name_map_[key]);
+        os << value;
+        os.close();
     }
 };
 
