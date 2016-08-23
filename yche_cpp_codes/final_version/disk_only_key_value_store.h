8bb5fd2c4bee104b62d0c93d1294fdcb3e638bcb
cheyulin <yche@cse.ust.hk>
2016-08-23 16:34:48 +0800

    discard mmap

--- yche_cpp_codes/final_version/disk_only_key_value_store.h
+++ yche_cpp_codes/final_version/disk_only_key_value_store.h
@@ -9,12 +9,6 @@
 #include <algorithm>
 #include <string>
 #include <fstream>
-#include <cstring>
-
-#include <cstring>
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 #define INDEX_FILE_NAME "index.meta"
 #define DB_NAME "value.db"
@@ -25,12 +19,12 @@ class Answer {
 private:
     unordered_map<string, pair<int, int>> key_index_info_map_;
     fstream key_index_stream_;
-    int db_file_descriptor_;
-    char *db_mmap_;
+    fstream db_stream_;
 
     int prefix_sum_index_{0};
     int length_{0};
 
+    char *value_buffer;
     bool is_first_in_{true};
 
     inline void read_index_info() {
@@ -65,15 +59,16 @@ private:
         }
     }
 
-
 public:
     Answer() {
+        value_buffer = new char[1024 * 32];
         key_index_stream_.open(INDEX_FILE_NAME, ios::in | ios::out | ios::app | ios::binary);
+        db_stream_.open(DB_NAME, ios::in | ios::out | ios::app | ios::binary);
         read_index_info();
-        db_file_descriptor_ = open(DB_NAME, O_RDWR | O_CREAT, 0600);
-        if (is_first_in_)
-            ftruncate(db_file_descriptor_, 6000000);
-        db_mmap_ = (char *) mmap(0, 6000000, PROT_READ | PROT_WRITE, MAP_SHARED, db_file_descriptor_, 0);
+    }
+
+    virtual ~Answer() {
+        delete[] value_buffer;
     }
 
     inline string get(string key) {
@@ -82,7 +77,9 @@ public:
         }
         else {
             auto &index_pair = key_index_info_map_[key];
-            return string(db_mmap_ + index_pair.first, 0, index_pair.second);
+            db_stream_.seekg(index_pair.first, ios::beg);
+            db_stream_.read(value_buffer, index_pair.second);
+            return string(value_buffer, 0, index_pair.second);
         }
     }
 
@@ -96,7 +93,8 @@ public:
         key_index_stream_ << prefix_sum_index_ << "\n";
         key_index_stream_ << length_ << "\n" << flush;
 
-        memcpy(db_mmap_ + prefix_sum_index_, value.c_str(), length_);
+        db_stream_.seekp(0, ios::end);
+        db_stream_ << value << flush;
 
         key_index_info_map_[key] = make_pair(prefix_sum_index_, length_);
         prefix_sum_index_ += length_;
